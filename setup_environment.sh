8ab21b7a399593da14581108c00ded2d240967a5
suhyeok24 <jasuhe900@korea.ac.kr>
2024-09-15 14:22:34 +0000

    add setup_environment.sh

---
+++ setup_environment.sh
@@ -0,0 +1,3 @@
+conda install pytorch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+python -m pip install timm==0.9.16 transformers 
+python -m pip install numpy pandas scipy statsmodels matplotlib tqdm einops prettytable tensorboardX
